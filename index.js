91e2cba59b25527dedcbb79240a098724d2f9353
Delta-Sine <d@delta-sine.org>
2016-04-06 19:18:28 -0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var qs = require('querystring');
-var our_query = window.location.search.slice(1);
-window.onload = function(){
-	var x = qs.parse(our_query);
-	console.log(x);
-	document.getElementById('content').innerHTML = JSON.stringify(x)
-}
