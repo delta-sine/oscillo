6ea8a97c60db11dc2f9de79d9b8afe1ad903b591
root <d@delta-sine.org>
2016-04-08 10:33:40 -0700

    WebRTC fixes

--- bundle.js
+++ bundle.js
@@ -1,63 +1,69 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/*
-  WaveForm Oscilloscope
-*/
-
+navigator.getUserMedia = (navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia);
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var distortion = audioCtx.createWaveShaper();
+var gainNode = audioCtx.createGain();
+var biquadFilter = audioCtx.createBiquadFilter();
+var convolver = audioCtx.createConvolver();
+var soundSource, concertHallBuffer, source, stream;
+var mute = document.querySelector('.mute');
+//Analyzer context
 var analyser = audioCtx.createAnalyser();
-
-//Setup the buffer
-analyser.fftSize = 2048;
-var bufferLength = analyser.frequencyBinCount;
-var dataArray = new Uint8Array(bufferLength);
-
-//Clear canvas
-canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-
-//Define draw()
-function draw() {
-  //requestAnimationFrame() keeps looping the draw() func once it's been started
-  drawVisual = requestAnimationFrame(draw);
-  //get time domain data, copy to dataArray
-  analyser.getByteTimeDomainData(dataArray);
-  //fill canvas with a solid color to start
-  canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-  canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-  //Set a line width and stroke colour for the wave we will draw, then begin drawing a path
-  canvasCtx.lineWidth = 2;
-  canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-  canvasCtx.beginPath();
-  /*
-  Determine the width of each segment of the line,
-   by dividing the canvas width by the array length
-  (equal to the FrequencyBinCount, as defined earlier on),
-  then define an x variable for the position to move each segment of the line.
-  */
-
-  var sliceWidth = WIDTH * 1.0 / bufferLength;
-  var x = 0;
-
-  /*
-    Now we run through a loop,
-      defining the position of a small segment of the wave for each point in the buffer
-        at a certain height based on the data point value form the array,
-        then moving the line across to the place where the next wave segment should be drawn:
-  */
-  for(var i = 0; i < bufferLength; i++) {
-        var v = dataArray[i] / 128.0;
-        var y = v * HEIGHT/2;
-        if(i === 0) {canvasCtx.moveTo(x, y);
-        } else {canvasCtx.lineTo(x, y);}
-        x += sliceWidth;
-      }
-  /*
-  Finally, we finish the line in the middle of the right hand side of the canvas,
-  then draw the stroke we've defined
-  */
-  canvasCtx.lineTo(canvas.width, canvas.height/2);
-  canvasCtx.stroke();
-}; //End draw();
-
-draw();
+analyser.minDecibels = -90;
+analyser.maxDecibels = -10;
+analyser.smoothingTimeConstant = 0.85;
+//Canvas context for visualizer
+var drawVisual;
+var canvas = document.querySelector('#scope');
+var canvasCtx = canvas.getContext("2d");
+var intendedWidth = document.querySelector('.wrapper').clientWidth;
+canvas.setAttribute('width',intendedWidth);
+//general purpose draw() function
+function draw(target_canvas) {
+    drawVisual = requestAnimationFrame(draw);
+    analyser.getByteTimeDomainData(dataArray);
+    target_canvas.fillStyle = 'rgb(200, 200, 200)'; //Foreground
+    target_canvas.fillRect(0, 0, WIDTH, HEIGHT);
+    target_canvas.lineWidth = 2;
+    target_canvas.strokeStyle = 'rgb(0, 0, 0)'; //Line color
+    target_canvas.beginPath();
+    var sliceWidth = WIDTH * 1.0 / bufferLength;
+    var x = 0;
+    for(var i = 0; i < bufferLength; i++) {
+      var v = dataArray[i] / 128.0;
+      var y = v * HEIGHT/2;
+      if(i === 0) {target_canvas.moveTo(x, y);}
+      else {target_canvas.lineTo(x, y);}
+      x += sliceWidth;
+    }
+    target_canvas.lineTo(canvas.width, canvas.height/2);
+    target_canvas.stroke();
+};
+//main block for RECORDING INPUT SIGNAL
+if (navigator.getUserMedia) {
+   console.log('getUserMedia supported.');
+   navigator.getUserMedia ({audio: true},
+      function(stream) {
+         source = audioCtx.createMediaStreamSource(stream);
+         source.connect(analyser);
+         analyser.connect(distortion);
+         gainNode.connect(audioCtx.destination);
+      	 visualize();
+      },
+      function(err) {console.log('The following gUM error occured: ' + err);}
+   );
+} else {console.log('getUserMedia not supported on your browser!');}
+function visualize() {
+  WIDTH = canvas.width;
+  HEIGHT = canvas.height;
+  analyser.fftSize = 2048;
+  var bufferLength = analyser.fftSize;
+  var dataArray = new Uint8Array(bufferLength);
+  canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+  draw(canvasCtx);
+}
 
 },{}]},{},[1]);
--- oscillo.js
+++ oscillo.js
@@ -2,45 +2,25 @@ navigator.getUserMedia = (navigator.getUserMedia ||
                           navigator.webkitGetUserMedia ||
                           navigator.mozGetUserMedia ||
                           navigator.msGetUserMedia);
+//Audio Context
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var distortion = audioCtx.createWaveShaper();
 var gainNode = audioCtx.createGain();
 var biquadFilter = audioCtx.createBiquadFilter();
 var convolver = audioCtx.createConvolver();
 var soundSource, concertHallBuffer, source, stream;
-var mute = document.querySelector('.mute');
 //Analyzer context
 var analyser = audioCtx.createAnalyser();
 analyser.minDecibels = -90;
 analyser.maxDecibels = -10;
 analyser.smoothingTimeConstant = 0.85;
-//Canvas context for visualizer
-var drawVisual;
+//Canvas context
 var canvas = document.querySelector('#scope');
 var canvasCtx = canvas.getContext("2d");
 var intendedWidth = document.querySelector('.wrapper').clientWidth;
+var drawVisual;
 canvas.setAttribute('width',intendedWidth);
-//general purpose draw() function
-function draw(target_canvas) {
-    drawVisual = requestAnimationFrame(draw);
-    analyser.getByteTimeDomainData(dataArray);
-    target_canvas.fillStyle = 'rgb(200, 200, 200)'; //Foreground
-    target_canvas.fillRect(0, 0, WIDTH, HEIGHT);
-    target_canvas.lineWidth = 2;
-    target_canvas.strokeStyle = 'rgb(0, 0, 0)'; //Line color
-    target_canvas.beginPath();
-    var sliceWidth = WIDTH * 1.0 / bufferLength;
-    var x = 0;
-    for(var i = 0; i < bufferLength; i++) {
-      var v = dataArray[i] / 128.0;
-      var y = v * HEIGHT/2;
-      if(i === 0) {target_canvas.moveTo(x, y);}
-      else {target_canvas.lineTo(x, y);}
-      x += sliceWidth;
-    }
-    target_canvas.lineTo(canvas.width, canvas.height/2);
-    target_canvas.stroke();
-};
+
 //main block for RECORDING INPUT SIGNAL
 if (navigator.getUserMedia) {
    console.log('getUserMedia supported.');
@@ -50,17 +30,36 @@ if (navigator.getUserMedia) {
          source.connect(analyser);
          analyser.connect(distortion);
          gainNode.connect(audioCtx.destination);
-      	 visualize();
+      	 draw();
       },
       function(err) {console.log('The following gUM error occured: ' + err);}
    );
 } else {console.log('getUserMedia not supported on your browser!');}
-function visualize() {
-  WIDTH = canvas.width;
-  HEIGHT = canvas.height;
-  analyser.fftSize = 2048;
-  var bufferLength = analyser.fftSize;
-  var dataArray = new Uint8Array(bufferLength);
-  canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-  draw(canvasCtx);
-}
+
+//general purpose draw() function
+function draw() {
+    WIDTH = canvas.width;
+    HEIGHT = canvas.height;
+    analyser.fftSize = 2048;
+    var dataArray = new Uint8Array(analyser.fftSize);
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    drawVisual = requestAnimationFrame(draw);
+    analyser.getByteTimeDomainData(dataArray);
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)'; //Foreground
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)'; //Line color
+    canvasCtx.beginPath();
+    var sliceWidth = WIDTH * 1.0 / analyser.fftSize;
+    var x = 0;
+    for(var i = 0; i < analyser.fftSize; i++) {
+      var v = dataArray[i] / 128.0;
+      var y = v * HEIGHT/2;
+      if(i === 0) {canvasCtx.moveTo(x, y);}
+      else {canvasCtx.lineTo(x, y);}
+      x += sliceWidth;
+    }
+    canvasCtx.lineTo(canvas.width, canvas.height/2);
+    canvasCtx.stroke();
+};
